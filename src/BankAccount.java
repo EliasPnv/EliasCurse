569c4e00b97cbfb17247d1c8cb4799e775290616
EliasP <elias.panov@mail.ru>
2022-09-12 17:19:52 +0300

    text messages added

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -7,10 +7,12 @@ public class BankAccount {
 
     public void deposit(double amount) { //внесение средств
         balance = balance + amount;
+        System.out.println("Внесено: " + amount);
     }
 
     public void withdraw(double amount) { //снятие средств
         balance = balance - amount;
+        System.out.println("Снято со счёта: " + amount);
     }
 
     public void display() { //Вывод баланса
